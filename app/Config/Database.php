ecc4697455bc42a7999011253cdc70621299b970
jpadamuz <jpadamuz@ecoavantis.com>
2023-01-01 13:48:08 +0100

    upload database

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -32,10 +32,10 @@ class Database extends Config
      */
     public $default = [
         'DSN'      => '',
-        'hostname' => getenv('DB_HOST'),
-        'username' => getenv('DB_USER'),
-        'password' => getenv('DB_PASSWORD'),
-        'database' => getenv('DB_DATABASE'),
+        'hostname' => env('DB_HOST'),
+        'username' => env('DB_USER'),
+        'password' => env('DB_PASSWORD'),
+        'database' => env('DB_DATABASE'),
         'DBDriver' => 'MySQLi',
         'DBPrefix' => '',
         'pConnect' => false,
