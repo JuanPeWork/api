53adc7d2b030ae197bf34bc39767ef1214d01cfc
jpadamuz <jpadamuz@ecoavantis.com>
2023-01-28 17:52:03 +0100

    disable option cors

--- app/Filters/Cors.php
+++ app/Filters/Cors.php
@@ -13,9 +13,9 @@ Class Cors implements FilterInterface
         header("Access-Control-Allow-Headers: X-API-KEY, Origin, X-Requested-With, Content-Type, Accept, Access-Control-Request-Method, Authorization");
         header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
         $method = $_SERVER['REQUEST_METHOD'];
-         if ($method == "OPTIONS") {
+/*          if ($method == "OPTIONS") {
             die();
-        }
+        } */
     }
  
     public function after(RequestInterface $request, ResponseInterface $response, $arguments = null)
