3525951d26bead4a2c06a7b94d2fb4513968d3a0
jpadamuz <jpadamuz@ecoavantis.com>
2023-01-28 18:13:14 +0100

    update routes

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,8 +35,8 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->get('/', 'Home::index');
 $routes->options('/*', 'Options::index');
+$routes->get('/', 'Home::index');
 
 $routes->get('workout', 'Workout::index');
 $routes->get('workout/(:num)', 'Workout::show/$1');
