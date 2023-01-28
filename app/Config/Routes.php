5348da3c177503d7adb09800fbaba9af4484f8a4
jpadamuz <jpadamuz@ecoavantis.com>
2023-01-28 19:05:28 +0100

    update cors

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -35,7 +35,6 @@ $routes->set404Override();
 
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
-$routes->options('/*', 'Options::index');
 $routes->get('/', 'Home::index');
 
 $routes->get('workout', 'Workout::index');
