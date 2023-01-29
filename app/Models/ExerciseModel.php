5e9253c06586541a3a1de12198f57883e5e07875
jpadamuz <jpadamuz@ecoavantis.com>
2023-01-29 15:51:45 +0100

    updata cors

--- app/Models/ExerciseModel.php
+++ app/Models/ExerciseModel.php
@@ -7,14 +7,6 @@ class ExerciseModel extends Model {
 
     protected $table = 'exercise';
     protected $primaryKey = 'id';
-    protected $allowedFields = ['id','training_session_id','name','sets','repts','weight','created_at','updated_at'];
-
-    public function __construct(){
-		parent ::__construct();
-        // $this->load->library('json');
-		//$this->load->database();
-        // $this->load->helper('url');
-    }
 
     public function getExercises() {
         return $this->db->table($this->table)->get()->getResult();
